04887b2ba5dc4b9404ce51d9c4d73ca4fe463560
Kaushik Ghose <kaushik.ghose@gmail.com>
2011-11-07 10:03:46 -0500

    Hover selection working well

--- photoribbon.cpp
+++ photoribbon.cpp
@@ -1,27 +1,37 @@
 #include "photoribbon.h"
 
-RibbonPhoto::RibbonPhoto(unsigned int tile_width) : QGraphicsRectItem()
+RibbonTile::RibbonTile(unsigned int tile_width) : QGraphicsRectItem()
 {
-    setRect(0,0,tile_width,tile_width);
-    //setBrush(Qt::white);
-    setFlag(QGraphicsItem::ItemIsSelectable);
+    state = NORMAL;
+    setRect(0,0,.75*tile_width,tile_width);
+    setFlag(QGraphicsItem::ItemIsSelectable);//Vital for rubberband selection
+    setAcceptHoverEvents(true);//This is how we preview
 }
 
-void RibbonPhoto::paint(QPainter *painter,
+void RibbonTile::paint(QPainter *painter,
            const QStyleOptionGraphicsItem *option,
            QWidget *widget)
 {
     if(isSelected())
-        setBrush(Qt::gray);
-    else
         setBrush(Qt::white);
+    else if(state==PREVIEW)
+        setBrush(Qt::blue);
+    else
+        setBrush(Qt::gray);
+
     QGraphicsRectItem::paint(painter, option, widget);
+}
+
+void RibbonTile::hoverEnterEvent(QGraphicsSceneHoverEvent * event)
+{
+    emit preview(this);
 }
 
 PhotoRibbon::PhotoRibbon(QObject *parent) :
     QGraphicsScene(parent)
 {
     tile_width = 10;
+    preview_tile = NULL;
     setBackgroundBrush(Qt::black);
 }
 
@@ -30,10 +40,21 @@ void PhotoRibbon::set_ids(QList<unsigned int> ids)
     QList<unsigned int>::iterator i;
     int x = 0;
     for (i = ids.begin(); i != ids.end(); ++i) {
-        RibbonPhoto *rp = new RibbonPhoto(tile_width);
-        rp->set_id(*i);
-        rp->setPos(x,0);
-        addItem((QGraphicsItem *)rp);
+        RibbonTile *rt = new RibbonTile(tile_width);
+        rt->set_id(*i);
+        rt->setPos(x,0);
+        addItem((QGraphicsItem *)rt);
+        QObject::connect(rt, SIGNAL(preview(RibbonTile *)),
+                this, SLOT(set_preview_tile(RibbonTile *)));
+
         x += tile_width;
     }
+}
+
+void PhotoRibbon::set_preview_tile(RibbonTile *prt)
+{
+    if(preview_tile != NULL)
+        preview_tile->unset_preview();
+    preview_tile = prt;
+    preview_tile->set_preview();
 }
--- photoribbon.h
+++ photoribbon.h
@@ -3,16 +3,29 @@
 
 #include <QtGui>
 
-class RibbonPhoto : public QGraphicsRectItem
+enum RibbonTileState {NORMAL, PREVIEW};
+class RibbonTile : public QObject, public QGraphicsRectItem
 {
+    Q_OBJECT
+
     unsigned int id;
+    RibbonTileState state;
+
 public:
-    explicit RibbonPhoto(unsigned int tile_width);
+    explicit RibbonTile(unsigned int tile_width);
     void set_id(unsigned int c_id) {id=c_id;}
     unsigned int get_id() {return id;}
     void paint(QPainter *painter,
                const QStyleOptionGraphicsItem *option,
                QWidget *widget);
+    void set_preview() {state=PREVIEW;}
+    void unset_preview() {state=NORMAL;}
+
+private:
+    void hoverEnterEvent(QGraphicsSceneHoverEvent * event);
+
+signals:
+    void preview(RibbonTile *);
 };
 
 class PhotoRibbon : public QGraphicsScene
@@ -20,6 +33,7 @@ class PhotoRibbon : public QGraphicsScene
     Q_OBJECT
 
     unsigned int item_count, tile_width;
+    RibbonTile *preview_tile;
 
 public:
     explicit PhotoRibbon(QObject *parent = 0);
@@ -28,6 +42,7 @@ public:
 signals:
 
 public slots:
+    void set_preview_tile(RibbonTile *);
 };
 
 #endif // PHOTORIBBON_H
